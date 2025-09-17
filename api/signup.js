10e11fea98d83159e6124b6fa6864bab4cd8488c
farleisx <laroxmafox@gmail.com>
2025-09-17 20:40:30 +0300

    Create signup.js

---
+++ api/signup.js
@@ -0,0 +1,20 @@
+import fs from 'fs';
+import bcrypt from 'bcrypt';
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') return res.status(405).json({ message: 'Method not allowed' });
+
+  const { username, password } = req.body;
+  if (!username || !password) return res.status(400).json({ message: 'Username & password required' });
+
+  let users = [];
+  try { users = JSON.parse(fs.readFileSync('users.json')); } catch {}
+
+  if (users.find(u => u.username === username)) return res.status(400).json({ message: 'User exists' });
+
+  const hashed = await bcrypt.hash(password, 10);
+  users.push({ username, password: hashed });
+  fs.writeFileSync('users.json', JSON.stringify(users, null, 2));
+
+  res.status(200).json({ message: 'User created' });
+}
